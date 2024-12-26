fa37fbf9afcbf7db59b26675a06a763b762dff73
SpiralP <spiral.palmer@gmail.com>
2024-12-26 14:18:15 -0800

    nix: use name/version from manifest and commit hash in version; use allowBuiltinFetchGit so we don't have to manage hashes

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs }:
     let
       inherit (nixpkgs) lib;
+
+      rustManifest = lib.importTOML ./Cargo.toml;
 
       makePackages = (system: dev:
         let
@@ -15,29 +17,20 @@
         in
         {
           default = pkgs.rustPlatform.buildRustPackage {
-            name = "classicube-auto-sprint-plugin";
-            src = lib.cleanSourceWith {
-              src = ./.;
-              filter = path: type:
-                lib.cleanSourceFilter path type
-                && (
-                  lib.any (re: builtins.match re (lib.removePrefix (builtins.toString ./.) (builtins.toString path)) != null) [
-                    "/Cargo.toml"
-                    "/Cargo.lock"
-                    "/\.cargo"
-                    "/\.cargo/.*"
-                    "/src"
-                    "/src/.*"
-                  ]
-                );
-            };
+            pname = rustManifest.package.name;
+            version = "${rustManifest.package.version}-${self.shortRev or self.dirtyShortRev}";
+
+            src = lib.sourceByRegex ./. [
+              "^\.cargo(/.*)?$"
+              "^build\.rs$"
+              "^Cargo\.(lock|toml)$"
+              "^cef_interface(/.*)?$"
+              "^src(/.*)?$"
+            ];
 
             cargoLock = {
               lockFile = ./Cargo.lock;
-              outputHashes = {
-                "async-dispatcher-0.1.0" = "sha256-rqpQ176/PnI9vvPrwQvK3GJbryjb3hHkb+o1RyCZ3Vg=";
-                "classicube-helpers-3.0.0+classicube.1.3.7" = "sha256-3hWKS6NmAH0x+SOi/nBKJLIQi/3ilG7WSRrPvF++wGE=";
-              };
+              allowBuiltinFetchGit = true;
             };
 
             nativeBuildInputs = with pkgs; [
